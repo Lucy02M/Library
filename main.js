dde874de13e66b5141ea7b5aa4bbbf7766b9491d
lucym19 <luciana.moraez09@gmail.com>
2023-01-04 20:33:30 -0300

    display book

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
 let myLibrary = [];
 
-function Book(name, author, pages, read) {
+function Book(name, author) {
     this.name = name
     this.author = author
-    this.pages = pages
-    this.read = read
 }
 
 function addBookToLibrary() {
-    myLibrary.push(Book);
+    const newBook = new Book($name.value, $author.value);
+    myLibrary.push(newBook);
+}
+
+function displayBook() {
+    const getBook = document.getElementById('getBook');
+    const nameInp = document.getElementById('bookName');
+    const authorInp = document.getElementById('author');
+    const showBook =document.getElementById('showName');
+    const showAuthor =document.getElementById('showAuthor');
+    showBook.innerHTML = nameInp.value;
+    showAuthor.innerHTML = authorInp.value;
+    nameInp.value = '';
+    author.value = '';
 }
